0d47205001b93089cdbe10186390254e97334a19
vikshree <40926569+vikshree@users.noreply.github.com>
2026-06-15 00:39:56 +0000

    Deploying to gh-pages from @ vikshree/vikshree.github.io@91a7f56f2ca42b97f8e6a697514753e1fed32775 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -489,9 +489,9 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
-            },},{id: "research-longe-range-3d-object-detection-and-tracking",
-          title: 'Longe-range 3D Object Detection and Tracking',
-          description: "To do: Developing advanced sensor fusion techniques for soft robotic manipulators to enable gentle and precise fruit harvesting in agricultural applications.",
+            },},{id: "research-long-range-3d-object-detection-and-tracking",
+          title: 'Long-range 3D Object Detection and Tracking',
+          description: "Stereo cameras provide dense 3D information of the surrounding environment, on the order of tens of millions of points per second. I proposed a bird&#39;s-eye-view (BEV) sensor model for stereo cameras that enables 3D detection of small objects at long range. The sensor model output was consumed by a particle filter, demonstrating real-time tracking of a 16 cm tall object at 160 m distance from a moving ego vehicle.",
           section: "Research",handler: () => {
               window.location.href = "/research/3d-detection-tracking/";
             },},{id: "research-human-attribute-identification-and-tracking",
