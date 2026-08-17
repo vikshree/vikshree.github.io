424521e1a97e3386b038ef8496b441f9ab0bffb0
vikshree <40926569+vikshree@users.noreply.github.com>
2026-08-17 02:57:20 +0000

    Deploying to gh-pages from @ vikshree/vikshree.github.io@f3390f1ba77eb0d0e83d33f987a9963e25bc2644 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,6 +30,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
+        },{id: "nav-personal",
+          title: "Personal",
+          description: "A few things I enjoy away from the desk.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/personal/";
+          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
